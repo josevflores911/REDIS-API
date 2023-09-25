5c003e268976c669f19af2f4ec986a0e10e2b81a
J <59713685+josevflores911@users.noreply.github.com>
2023-09-24 21:13:26 -0300

    usuarios y authenticacion part 1

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '30b9aa758bc68da15ac8e10c87e94a68',
+    expiresIn: '1d',
+  },
+};
--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,10 +1,14 @@
 import productRouter from '@modules/products/routes/products.routes';
+import sessionsRouter from '@modules/users/routes/sessions.routes';
+import userRouter from '@modules/users/routes/users.routes';
 import { Router } from 'express';
 import { json } from 'stream/consumers';
 
 const routes = Router();
 
 routes.use('/products', productRouter);
+routes.use('/users', userRouter);
+routes.use('/sessions', sessionsRouter);
 
 export default routes;
 
