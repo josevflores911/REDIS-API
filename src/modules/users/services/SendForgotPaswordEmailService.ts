a4e3919b131d24cd4b2a9a82927b6b2db6fa1ddc
J <59713685+josevflores911@users.noreply.github.com>
2023-10-13 10:42:14 -0300

    final gerenciamiento de senhas de usuarios

--- src/modules/users/services/SendForgotPaswordEmailService.ts
+++ src/modules/users/services/SendForgotPaswordEmailService.ts
@@ -3,6 +3,7 @@ import { getCustomRepository } from 'typeorm';
 import UserRepository from '../typeorm/repositories/UserRepository';
 import UserTokensRepository from '../typeorm/repositories/UserTokensRepository';
 import EtherealMail from '@config/mail/EtherealMail';
+import path from 'path';
 
 interface IRequest {
   email: string;
@@ -22,13 +23,30 @@ class SendForgotPasswordEmailService {
 
     console.log(user);
 
-    const token = await userTokensRepository.generate(user.id);
+    const { token } = await userTokensRepository.generate(user.id);
+
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs',
+    );
 
     console.log(token);
 
     await EtherealMail.sendMail({
-      to: email,
-      body: `Solicitacao de redefinicao de senha recebida: ${token?.token}`,
+      to: {
+        name: user.name,
+        email: user.email,
+      },
+      subject: '[API Vendas] Recuperacao de Senha',
+      templateData: {
+        file: `Ola {{name}}: {{token}}`,
+        variables: {
+          name: user.name,
+          link: `http://localhost:300/reset_password?token=${token}`,
+        },
+      },
     });
   }
 }
